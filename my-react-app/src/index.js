dfe090770be9922c3f6798cbb572bd6a1a3bc495
Felipe-LasHeras <FE.LASHERAS@DUOCUC.CL>
2025-05-13 01:05:22 -0400

    PAgina funciona pero desorden

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import ".//variables.css"
+import "./assets/fonts/fonts.css";
+import "./variables.css"
 
 createRoot(document.getElementById("app")).render(
   <StrictMode>
